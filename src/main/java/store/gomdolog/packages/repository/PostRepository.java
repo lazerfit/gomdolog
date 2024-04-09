1bc9431addd4af54a95555c2c53234a4f9eceb16
sg kim <redkafe98@gmail.com>
2024-04-10 04:47:15 +0900

    create: create post update method

--- src/main/java/store/gomdolog/packages/repository/PostRepository.java
+++ src/main/java/store/gomdolog/packages/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package store.gomdolog.packages.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import store.gomdolog.packages.domain.Category;
 import store.gomdolog.packages.domain.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    List<Post> findAllByCategory(Category category);
 }
