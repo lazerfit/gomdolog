361ee2cec6790d8b9040b7a9a85405c2609090d3
sg kim <redkafe98@gmail.com>
2024-06-24 23:24:31 +0900

    refactor: add constraint

--- src/main/java/store/gomdolog/packages/dto/UserSignInRequest.java
+++ src/main/java/store/gomdolog/packages/dto/UserSignInRequest.java
@@ -1,9 +1,10 @@
 package store.gomdolog.packages.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserSignInRequest(
-    @NotBlank String email,
+    @NotBlank @Email String email,
     @NotBlank String password
 ) {
 
