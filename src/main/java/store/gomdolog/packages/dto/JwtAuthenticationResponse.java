85423c639f2cabf6d158038ed70f57a636e8bed2
sg kim <redkafe98@gmail.com>
2024-06-24 23:25:34 +0900

    refactor: remove constraint

--- src/main/java/store/gomdolog/packages/dto/JwtAuthenticationResponse.java
+++ src/main/java/store/gomdolog/packages/dto/JwtAuthenticationResponse.java
@@ -1,9 +1,7 @@
 package store.gomdolog.packages.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record JwtAuthenticationResponse(
-    @NotBlank String token
+    String token
 ) {
 
 }
