5f3c53b8c68d55593e10e176b6e5e84d7ffb1b15
sg kim <redkafe98@gmail.com>
2024-06-24 23:17:11 +0900

    refactor: add constraints

--- src/main/java/store/gomdolog/packages/dto/PostUpdate.java
+++ src/main/java/store/gomdolog/packages/dto/PostUpdate.java
@@ -1,13 +1,15 @@
 package store.gomdolog.packages.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record PostUpdate(
-    Long id,
-    String title,
-    String content,
+    @NotEmpty Long id,
+    @NotBlank String title,
+    @NotBlank String content,
     String categoryTitle,
     List<String> tags
 ) {
