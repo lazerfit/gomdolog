09c12a826b80212b80f21eaba66c545b3b213053
sg kim <redkafe98@gmail.com>
2024-06-24 22:59:58 +0900

    refactor: remove unused annotation

--- src/main/java/store/gomdolog/packages/repository/PostRepository.java
+++ src/main/java/store/gomdolog/packages/repository/PostRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import store.gomdolog.packages.domain.Category;
 import store.gomdolog.packages.domain.Post;
 
-@Repository
 public interface PostRepository extends PostRepositoryCustom, JpaRepository<Post, Long> {
 
     List<Post> findAllByCategory(Category category);
