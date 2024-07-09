4fc76ad0ff651c6179f4350d94f652eedb28c372
sg kim <redkafe98@gmail.com>
2024-07-09 16:03:22 +0900

    refactor: test code

--- src/test/java/store/gomdolog/packages/service/PostTagServiceIntegrationTest.java
+++ src/test/java/store/gomdolog/packages/service/PostTagServiceIntegrationTest.java
@@ -5,19 +5,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import jakarta.transaction.Transactional;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import store.gomdolog.packages.domain.Category;
 import store.gomdolog.packages.domain.Post;
 import store.gomdolog.packages.domain.Tag;
 import store.gomdolog.packages.error.PostNotFound;
+import store.gomdolog.packages.repository.CategoryRepository;
 import store.gomdolog.packages.repository.PostRepository;
+import store.gomdolog.packages.repository.PostTagRepository;
 import store.gomdolog.packages.repository.TagRepository;
 
 @SpringBootTest
-@Sql(scripts = "/PostTagServiceIntegrationTest.sql", executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
 @Transactional
 class PostTagServiceIntegrationTest {
 
@@ -29,30 +31,63 @@ class PostTagServiceIntegrationTest {
 
     @Autowired
     private PostTagService postTagService;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private PostTagRepository postTagRepository;
+
+    @BeforeEach
+    void setUp() {
+        Category c1 = categoryRepository.save(new Category("spring"));
+        Category c2 = categoryRepository.save(new Category("css"));
+        Category c3 = categoryRepository.save(new Category("Spring"));
+
+        Post p1 = Post.builder().title("제목").content("<p>내용</p>").views(0L).category(c1)
+            .thumbnail("Default Thumbnail").build();
+        postRepository.save(p1);
+        Post p2 = Post.builder().title("제목2").content("<p>내용2</p>").views(0L).category(c2)
+            .thumbnail("Default Thumbnail").build();
+        postRepository.save(p2);
+        Post.builder().title("제목3").content("<p>내용3</p>").views(0L).category(c3)
+            .thumbnail("Default Thumbnail").build();
+        postRepository.save(p2);
+    }
+
+    @AfterEach
+    void tearDown() {
+        postTagRepository.deleteAll();
+        categoryRepository.deleteAll();
+        postRepository.deleteAll();
+        tagRepository.deleteAll();
+    }
 
     @Test
     void test1() {
-        Post post = postRepository.findById(3L).orElseThrow(PostNotFound::new);
-        Tag tag = tagRepository.findById(1L).orElseThrow(null);
+        Post post = postRepository.findAll().get(0);
+        Tag tag = tagRepository.save(new Tag("spring"));
 
         postTagService.save(post, List.of(tag));
 
-        Post savedPost = postRepository.findById(3L).orElseThrow(PostNotFound::new);
+        Post savedPost = postRepository.findById(post.getId()).orElseThrow(PostNotFound::new);
 
         assertThat(savedPost.getPostTags().get(0).getTag()).isEqualTo(tag);
     }
 
     @Test
     void test2() {
-        postTagService.delete(2L);
+        Post post = postRepository.findAll().get(0);
+        Tag tag = tagRepository.save(new Tag("spring"));
 
-        Post post = postRepository.findById(2L).orElseThrow(PostNotFound::new);
-        assertThat(post.getPostTags()).isEmpty();
+        postTagService.save(post, List.of(tag));
+        postTagService.delete(post.getId());
+
+        Post foundPost = postRepository.findById(post.getId()).orElseThrow(PostNotFound::new);
+        assertThat(foundPost.getPostTags()).isEmpty();
     }
 
     @Test
     void test3() {
-        assertThatThrownBy(() -> postTagService.delete(3L))
+        assertThatThrownBy(() -> postTagService.delete(1000L))
             .isInstanceOf(PostNotFound.class)
             .hasMessage("해당 post가 존재하지 않습니다.");
     }
--- src/test/resources/PostTagServiceIntegrationTest.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO category (title) values ( 'c1' ),( 'c2' ),('c3');
-INSERT INTO post (category_id, created_date, modified_date, views, thumbnail, title, content, is_deleted) values
-(1,CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP() ,  0, 'Default Thumbnail', '제목', '<p>내용</p>', false  ),
-(2,CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP() ,  1, 'Default Thumbnail', '제목2', '<p>내용2</p>', false  ),
-(2,CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP() , 2, 'Default Thumbnail', '제목3', '<p>내용3</p>', true  );
-INSERT INTO tag (name) values ('t1' ), ('t2');
-INSERT INTO post_tag (post_id, tag_id) values ( 1, 1 ), (2,2);
